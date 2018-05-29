69b44b8c85635dda6537eae5845ff5b23470c706
Jose Moreno <aldahirmoreno@gmail.com>
2018-05-28 22:17:04 -0700

    Styling for individual weekdays

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,8 +1,12 @@
 var date = moment();
-const today = document.querySelector('#day-today');
 
 const body = document.querySelector("body");
 body.style.height = `${screen.height}px`; 
+
+
+/** FINDING OUT RANGES FOR WEEK **/ 
+
+const today = document.querySelector('#day-today');
 
 const start_date = date
     .startOf("isoweek")
@@ -14,3 +18,36 @@ const end_Date = date
   .format("MMMM Do");
 
 today.innerHTML = `${start_date} - ${end_Date}`;
+
+/** FINDING OUT RANGES FOR WEEK **/
+
+/** IPUTTING DATES INDIVIDUALLY **/
+
+const weekdays = document.querySelectorAll("#weekday");
+
+const weekdays_moment = [];
+
+function weekday_start_date(day = 0) {
+    return date.startOf("isoweek").add("days", day).format("MMMM Do");
+}
+
+for (let i = 0; weekdays_moment[weekdays_moment.length - 1] !== end_Date; i++) {
+    weekdays_moment.push(weekday_start_date(i));
+}
+
+weekdays.forEach((day, i) => {
+    day.innerHTML = weekdays_moment[i];
+})
+
+/** IPUTTING DATES INDIVIDUALLY **/
+
+
+/** DISPLAYING WHAT DAY TODAY IS*/
+
+weekdays.forEach((day, i) => {
+    if (day.innerHTML === `${moment().format("MMMM Do")}`) {
+        day.parentNode.style.backgroundColor = "#d3d3d3";
+    } 
+});
+
+/** DISPLAYING WHAT DAY TODAY IS*/
