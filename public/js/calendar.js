fb065803fb4b10d01e07a3e1f6e5ba6d32d20914
Jose Moreno <aldahirmoreno@gmail.com>
2018-05-28 21:36:19 -0700

    Style changes

---
+++ public/js/calendar.js
@@ -0,0 +1,6 @@
+var date = moment();
+const new_weekday = document.querySelector('#weekday');
+// new_weekday.innerHTML = 'THISNEWNEW BOIZ'
+
+const body = document.querySelector("body");
+body.style.height = `${screen.height}px`; 
