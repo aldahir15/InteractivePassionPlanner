3f3b18f0fc60cc6828468eefde468df117c6070a
Jose Moreno <aldahirmoreno@gmail.com>
2018-05-28 21:53:09 -0700

    Add implementation of week days

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,6 +1,16 @@
 var date = moment();
-const new_weekday = document.querySelector('#weekday');
-// new_weekday.innerHTML = 'THISNEWNEW BOIZ'
+const today = document.querySelector('#day-today');
 
 const body = document.querySelector("body");
 body.style.height = `${screen.height}px`; 
+
+const start_date = date
+    .startOf("isoweek")
+    .format("MMMM Do");
+
+const end_Date = date
+  .startOf("isoweek")
+  .add("days", 6)
+  .format("MMMM Do");
+
+today.innerHTML = `${start_date} - ${end_Date}`;
